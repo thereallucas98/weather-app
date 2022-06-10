80dc13e00b083240e950cb34f85431258aec3c83
david.santos <david.santos@leanwork.com.br>
2022-06-09 22:48:24 -0300

    added styled components

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    primary: "#A370F7",
+    primary_light: "rgba(163, 112, 247, 0.5)",
+    secondary: "#FF872C",
+    secondary_light: "rgba(255, 135, 44, 0.3)",
+    success: "#12A454",
+    success_light: "rgba(18, 164, 84, 0.5)",
+    attention: "#E83F5B",
+    attention_light: "rgba(232, 63, 91, 0.5)",
+    shape: "#FFF",
+    title: "#fafafa",
+    text: "#4d4d4d",
+    text_dark: "#000000",
+    background: "#121015",
+    background_light: "#1a1a1a",
+  },
+
+  fonts: {
+    regular: "Roboto_400Regular",
+    medium: "Roboto_500Medium",
+    bold: "Roboto_700Bold",
+  },
+};
