b0f2e0357f4211b355c02dd60183df3e2fc06a97
thereallucas98 <lucas1998david@gmail.com>
2022-06-10 22:47:20 -0300

    Created Weather Card Styles - need to migrade to a Isolated component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,11 +6,35 @@ function Home() {
   return (
     <HomeUI.Container>
       <HomeUI.Header>
-        <HomeUI.Greetings>Olá, David Lucas</HomeUI.Greetings>
+        <HomeUI.Greetings>Olá David Lucas, 🤞</HomeUI.Greetings>
         <HomeUI.ProfileImage
           source={{ uri: "https://github.com/thereallucas98.png" }}
         />
       </HomeUI.Header>
+
+      <HomeUI.SearchWrapper>
+        <HomeUI.SearchIcon name="search" size={32} color="#FFF" />
+        <HomeUI.SearchInput
+          placeholder="Busque por uma cidade"
+          placeholderTextColor="#FFF"
+        />
+      </HomeUI.SearchWrapper>
+
+      <HomeUI.WeatherCardWrapper>
+        <HomeUI.WeatherImageStatus
+          name="weather-partly-lightning"
+          size={120}
+          color="#FFF"
+        />
+        <HomeUI.WeatherLocationContent>
+          <HomeUI.TemperatureValue>19°C</HomeUI.TemperatureValue>
+          <HomeUI.LocationLabelWrapper>
+            <HomeUI.LocationIcon name="location-pin" size={20} color="#FFF" />
+            <HomeUI.LocationCity>Helsinki,</HomeUI.LocationCity>
+            <HomeUI.LocationCountry>Finland</HomeUI.LocationCountry>
+          </HomeUI.LocationLabelWrapper>
+        </HomeUI.WeatherLocationContent>
+      </HomeUI.WeatherCardWrapper>
     </HomeUI.Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,8 @@
-import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import Feather from "@expo/vector-icons/Feather";
+import { RFValue } from "react-native-responsive-fontsize";
+import theme from "../../global/styles/theme";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -22,14 +25,100 @@ const Greetings = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
 `;
 
-const ProfileImage = styled.Image.attrs(({ theme }) => ({
+const ProfileImage = styled.Image.attrs({
   borderColor: theme.colors.primary,
   borderWidth: 1,
-}))`
+})`
   height: 60px;
   width: 60px;
 
   border-radius: 30px;
 `;
 
-export { Container, Header, Greetings, ProfileImage };
+const SearchWrapper = styled.View`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background_light};
+  padding: 10px;
+  border-radius: 8px;
+  margin-top: 32px;
+  margin-bottom: 12px;
+  flex-direction: row;
+`;
+
+const SearchIcon = styled(Feather)``;
+
+const SearchInput = styled.TextInput`
+  width: 90%;
+  height: 42px;
+  padding: 10px;
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(14)}px;
+`;
+
+const WeatherCardWrapper = styled.View`
+  width: 100%;
+  height: 160px;
+
+  padding: 10px;
+  margin-top: 32px;
+
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const WeatherImageStatus = styled(MaterialCommunityIcons)``;
+
+const WeatherLocationContent = styled.View``;
+
+const TemperatureValue = styled.Text`
+  font-size: ${RFValue(42)}px;
+  font-family: "Roboto_700Bold";
+  color: ${({ theme }) => theme.colors.shape};
+`;
+
+const LocationLabelWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LocationIcon = styled(MaterialIcons)``;
+
+const LocationCity = styled.Text`
+  font-size: ${RFValue(14)}px;
+  font-family: "Roboto_700Bold";
+  color: ${({ theme }) => theme.colors.shape};
+  text-align: right;
+`;
+
+const LocationCountry = styled.Text`
+  margin-left: 2px;
+  font-size: ${RFValue(14)}px;
+  font-family: "Roboto_400Regular";
+  color: ${({ theme }) => theme.colors.shape};
+`;
+
+export {
+  Container,
+  Header,
+  Greetings,
+  ProfileImage,
+  SearchWrapper,
+  SearchIcon,
+  SearchInput,
+  WeatherCardWrapper,
+  WeatherImageStatus,
+  WeatherLocationContent,
+  TemperatureValue,
+  LocationIcon,
+  LocationLabelWrapper,
+  LocationCity,
+  LocationCountry,
+};
